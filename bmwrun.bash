f5d2476a474f65d0863528cc66fe075f295525cb
Bo E <boe@wengle.se>
2022-03-13 16:13:32 +0100

    test for pyrfid

---
+++ bmwrun.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+pyuic5 -o mw55b.py ui/mw55b.ui ; python app_mw55b.py
---
+++ dopath.bash
@@ -0,0 +1,2 @@
+PATH=/home/boe/git/python/qt5/tools/ve39qt5/lib/python3.9/site-packages/qt5_applications/Qt/bin:/home/boe/git/python/qt5/tools/ve39qt5/bin:/home/boe/bin:/home/boe/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
